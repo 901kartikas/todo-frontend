e0002272749adf50266881a0878ee64864765bd0
Sofiakh <49202583+901kartikas@users.noreply.github.com>
2021-02-02 15:07:05 +0700

    ok
    update

--- src/Component/AddTodo.js
+++ src/Component/AddTodo.js
@@ -1,45 +1,61 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Paper, Grid, Tooltip, Fab, Button } from '@material-ui/core';
+import { Paper, Grid, Tooltip, List, Button, ListItem, ListItemText} from '@material-ui/core';
 import ShowTodo from './ShowTodo'
 import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
   fab: {
     margin: theme.spacing(2),
- 
+
   },
-  btn:{
+  btn: {
     borderRadius: 40,
+  },
+  title: {
+    flexDirection: 'row',
+  },
+  garis:{
+    
   },
 }));
 
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 export default function SimplePaper() {
   const classes = useStyles();
 
   return (
-    
-      <Paper   >
-       <Grid container  spacing={2}>
-           <Grid item xs={4} >
-             <div>
-             <h1>Todo</h1>
-           <Tooltip title="Add" aria-label="add">
-             <Button className={classes.btn}>
-             <AddIcon style={{ fontSize: 30, color:"#000" }}/>
-             </Button>
-            </Tooltip>
-             </div>
-            
-          
-           </Grid>
-           <Grid item xs={8}   >
-             <ShowTodo/>
-             </Grid>
-       </Grid>
-      </Paper>
-   
-    
+
+    <Paper   >
+      <Grid container spacing={2}>
+        <Grid item xs={4} >
+          <div className={classes.title}>
+            <h1>Todo
+              <Tooltip title="Add" aria-label="add">
+                <Button className={classes.btn}>
+                  <AddIcon style={{ fontSize: 30, color: "#000" }} />
+                </Button>
+              </Tooltip>
+            </h1>
+          </div>
+          <List component="nav" aria-label="secondary mailbox folders">
+            <ListItem button className={classes.garis}>
+              <ListItemText primary="Trash" />
+            </ListItem>
+            <ListItemLink href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemLink>
+          </List>
+        </Grid>
+        <Grid item xs={8}   >
+          <ShowTodo />
+        </Grid>
+      </Grid>
+    </Paper>
+
+
   );
 }
